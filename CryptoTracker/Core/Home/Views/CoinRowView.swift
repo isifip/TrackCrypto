cbbfb0c1d42b677c7c101e49f0195b2173b1f686
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-31 17:17:44 +0400

    + PreviewProvider for CoinModel

---
+++ CryptoTracker/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,23 @@
+//
+//  CoinRowView.swift
+//  CryptoTracker
+//
+//  Created by Irakli Sokhaneishvili on 31.03.22.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    //let coin: CoinModel
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView()
+    }
+}
