07449cc45111fbd1c9fa9e2e7940333490572c1c
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-31 15:40:45 +0400

    + Custom Navigation header setup using CircleButtonView

--- CryptoTracker/Core/Home/Views/HomeView.swift
+++ CryptoTracker/Core/Home/Views/HomeView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
     var body: some View {
         ZStack {
             // Background Layer
@@ -16,7 +19,26 @@ struct HomeView: View {
             
             // Content Layer
             VStack {
-                Text("Header")
+                HStack {
+                    CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                        .animation(.none, value: showPortfolio)
+                    Spacer()
+                    Text(showPortfolio ? "Portfolio" : "Live Prices")
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.theme.accent)
+                        .animation(.none, value: showPortfolio)
+                    Spacer()
+                    CircleButtonView(iconName: "chevron.right")
+                        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                showPortfolio.toggle()
+                            }
+                        }
+                }
+                .padding(.horizontal)
+                
                 Spacer(minLength: 0)
             }
         }
