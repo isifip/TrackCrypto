c879182db0bb99803d41a805cd629f939b58d6b8
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-31 14:51:13 +0400

    first commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Irakli Sokhaneishvili on 31.03.22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
