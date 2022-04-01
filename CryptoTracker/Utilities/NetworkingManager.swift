51457f6ca95f5ae1f42a43b655650f025b1b858d
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-04-01 17:24:00 +0400

    + Reusable Networking Layer, making our download function more generic

---
+++ CryptoTracker/Utilities/NetworkingManager.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkingManager.swift
+//  CryptoTracker
+//
+//  Created by Irakli Sokhaneishvili on 01.04.22.
+//
+
+import Foundation
+import Combine
+
+class NetworkingManager {
+    static func download(url: URL) -> AnyPublisher<Data, Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap { output -> Data in
+                guard let response = output.response as? HTTPURLResponse,
+                      response.statusCode >= 200 && response.statusCode < 300 else {
+                    throw URLError(.badServerResponse)
+                }
+                return output.data
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+}
