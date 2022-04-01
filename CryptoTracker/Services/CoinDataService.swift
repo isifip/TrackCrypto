0e66e9ee9322340ff6e10c3ceeb69645d4e387df
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-04-01 17:10:05 +0400

    + Subscribing from HomeViewModel and Displaying results in HomeView

--- CryptoTracker/Services/CoinDataService.swift
+++ CryptoTracker/Services/CoinDataService.swift
@@ -11,6 +11,7 @@ import Combine
 class CoinDataService {
     
     @Published var allCoins: [CoinModel] = []
+    
     var coinSubscription: AnyCancellable?
     
     init () {
@@ -18,8 +19,8 @@ class CoinDataService {
     }
     
     private func getCoins() {
-        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currenct=usd&order=market_cap_desc&per_age=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
-        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
+
         coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
             .tryMap { output -> Data in
@@ -40,6 +41,7 @@ class CoinDataService {
                 }
             } receiveValue: { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
+                self?.coinSubscription?.cancel()
             }
     }
 }
