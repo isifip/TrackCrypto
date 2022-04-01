a7c50457b6e22aaef4e05f33e24ff61eb16cceb0
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-04-01 18:02:40 +0400

    + Downloading and Displaying CoinImages for each item

---
+++ CryptoTracker/Services/CoinImageService.swift
@@ -0,0 +1,36 @@
+//
+//  CoinImageService.swift
+//  CryptoTracker
+//
+//  Created by Irakli Sokhaneishvili on 01.04.22.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageService {
+    
+    @Published var image: UIImage? = nil
+    private let coin: CoinModel
+    
+    private var imageSubscription: AnyCancellable?
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        getCoinImage()
+    }
+    
+    private func getCoinImage() {
+        guard let url = URL(string: coin.image) else { return }
+
+        imageSubscription = NetworkingManager.download(url: url)
+            .tryMap({ data -> UIImage? in
+                return UIImage(data: data)
+            })
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedImage in
+                self?.image = returnedImage
+                self?.imageSubscription?.cancel()
+            })
+    }
+}
