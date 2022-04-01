c6769bb28adc31841f9ae185917710f8ac6162af
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-04-01 19:01:13 +0400

    + Downloading images to Filemanager, then display images that are already downloaded.

--- CryptoTracker/Utilities/LocalFileManager.swift
+++ CryptoTracker/Utilities/LocalFileManager.swift
@@ -19,16 +19,27 @@ class LocalFileManager {
     
     func saveImage(image: UIImage, imageName: String, folderName: String) {
         
+        // Create Folder
         createFolderIfNeeded(foldername: folderName)
         
+        // Get path for image
         guard let data = image.pngData() else { return }
         guard let url = getURLForImage(imageName: imageName, folderName: folderName) else { return }
         
+        // Save Image to path
         do {
             try data.write(to: url)
         } catch let error {
             print("Error saving image. imageName: \(imageName), \(error.localizedDescription)")
         }
+    }
+    
+    func getImage(imageName: String, folderName: String) -> UIImage? {
+        guard let url = getURLForImage(imageName: imageName, folderName: folderName),
+              FileManager.default.fileExists(atPath: url.path) else {
+            return nil
+        }
+        return UIImage(contentsOfFile: url.path)
     }
     
     private func createFolderIfNeeded(foldername: String) {
