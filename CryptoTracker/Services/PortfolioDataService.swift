0cce1cc624f8898d52b0a5fa269aa588d0bf137b
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-04-05 14:56:38 +0400

    + Data manipulation: add, save, update, delete

--- CryptoTracker/Services/PortfolioDataService.swift
+++ CryptoTracker/Services/PortfolioDataService.swift
@@ -28,5 +28,39 @@ class PortfolioDataService {
             print("Error fetching PortfolioEntities: \(error)")
         }
     }
+    
+    
+    private func add(coin: CoinModel, amount: Double) {
+        let entity = PortfolioEntity(context: container.viewContext)
+        entity.coinID = coin.id
+        entity.amount = amount
+        
+        applyChanges()
+    }
+    
+    private func update(entity: PortfolioEntity, amount: Double) {
+        entity.amount = amount
+        applyChanges()
+    }
+    
+    
+    private func delete(entity: PortfolioEntity) {
+        container.viewContext.delete(entity)
+    }
+    
+    private func save() {
+        do {
+            try container.viewContext.save()
+        } catch let error {
+            print("Error saving to Core Data: \(error )")
+        }
+    }
+    
+    private func applyChanges() {
+        save()
+        getPortfolio()
+    }
+    
+    
 }
 
