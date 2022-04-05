f15a555739454df677be11c78b0d0ede91b4c058
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-04-05 16:42:34 +0400

    + Displaying Portfolio coins to the view after saving.

--- CryptoTracker/Core/Home/Views/PortfolioView.swift
+++ CryptoTracker/Core/Home/Views/PortfolioView.swift
@@ -53,13 +53,13 @@ extension PortfolioView {
     private var coinLogoList: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             LazyHStack(spacing: 10) {
-                ForEach(vm.allCoins) { coin in
+                ForEach(vm.searchText.isEmpty ? vm.portfolioCoins : vm.allCoins) { coin in
                     CoinLogoView(coin: coin)
                         .frame(width: 70)
                         .padding(4)
                         .onTapGesture {
                             withAnimation(.easeIn) {
-                                selectedCoin = coin
+                                updateSelectedCoin(coin: coin)
                             }
                         }
                         .background(
@@ -70,6 +70,17 @@ extension PortfolioView {
             }
             .padding(.vertical, 4)
             .padding(.leading)
+        }
+    }
+    
+    private func updateSelectedCoin(coin: CoinModel) {
+        selectedCoin = coin
+        
+        if let portfolioCoin = vm.portfolioCoins.first(where: { $0.id == coin.id }),
+           let amount = portfolioCoin.currentHoldings {
+            quantityText = String(amount)
+        } else {
+            quantityText = ""
         }
     }
     
@@ -128,10 +139,14 @@ extension PortfolioView {
     }
     
     private func saveButtonPressed() {
-        guard let coin = selectedCoin else { return }
+        guard
+            let coin = selectedCoin,
+            let amount = Double(quantityText)
+        else { return }
         
         // Save to portfolio
-        
+        vm.updatePortfolio(coin: coin, amount: amount)
+    
         // Show checkmark
         withAnimation(.easeIn) {
             showCheckmark = true
