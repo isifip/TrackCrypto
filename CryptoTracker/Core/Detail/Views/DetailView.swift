5a33a1bccde55564d90be917a5f6d0243419c1fa
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-04-07 22:04:11 +0400

    + DetailLoadingView

--- CryptoTracker/Core/Detail/Views/DetailView.swift
+++ CryptoTracker/Core/Detail/Views/DetailView.swift
@@ -7,22 +7,35 @@
 
 import SwiftUI
 
-struct DetailView: View {
+struct DetailLoadingView: View {
     
     @Binding var coin: CoinModel?
     
-    init(coin: Binding<CoinModel?>) {
-        self._coin = coin
-        print("Initializing Detail View for \(coin.wrappedValue?.name)")
+    var body: some View {
+        ZStack {
+            if let coin = coin {
+                DetailView(coin: coin)
+            }
+        }
+    }
+}
+
+struct DetailView: View {
+    
+    let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        print("Initializing Detail View for \(coin.name)")
     }
     
     var body: some View {
-        Text(coin?.name ?? "")
+        Text(coin.name)
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(coin: .constant(dev.coin))
+        DetailView(coin: dev.coin)
     }
 }
