3e0e8f7523af3b024e011dea5984f2d32fa4849d
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-04-09 15:15:13 +0400

    + Starting and ending date formatting

--- CryptoTracker/Core/Detail/Views/ChartView.swift
+++ CryptoTracker/Core/Detail/Views/ChartView.swift
@@ -13,6 +13,8 @@ struct ChartView: View {
     let maxY: Double
     let minY: Double
     let lineColor: Color
+    let startingDate: Date
+    let endingDate: Date
     
     init(coin: CoinModel) {
         data = coin.sparklineIn7D?.price ?? []
@@ -21,17 +23,27 @@ struct ChartView: View {
         
         let priceChange = (data.last ?? 0) - (data.first ?? 0)
         lineColor = priceChange > 0 ? Color.theme.green : Color.theme.red
+        
+        endingDate = Date(coinGeckoString: coin.lastUpdated ?? "")
+        startingDate = endingDate.addingTimeInterval(-7 * 24 * 60 * 60)
     }
     
     var body: some View {
-        chartView
-            .frame(height: 200)
-            .background(
-                chartBackground
-            )
-            .overlay(alignment: .leading) {
-                chartYAxis
-            }
+        VStack {
+            chartView
+                .frame(height: 200)
+                .background(
+                    chartBackground
+                )
+                .overlay(alignment: .leading) {
+                    chartYAxis
+                }
+                .font(.caption)
+            chartDateLabels
+                .font(.subheadline)
+                .foregroundColor(.primary)
+        }
+        .foregroundColor(Color.theme.secondaryText)
     }
 }
 
@@ -83,6 +95,14 @@ extension ChartView {
             Text(((maxY + minY) / 2).formattedWithAbbreviations())
             Spacer()
             Text(minY.formattedWithAbbreviations())
+        }
+    }
+    
+    private var chartDateLabels: some View {
+        HStack {
+            Text(startingDate.asShortDateString())
+            Spacer()
+            Text(endingDate.asShortDateString())
         }
     }
 }
