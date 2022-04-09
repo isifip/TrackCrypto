68952e399982a19b8d5a606dff5ab2c0f755e2e8
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-04-09 13:09:18 +0400

    + Downloading details data and initialising its view model inside our view

--- CryptoTracker/Core/Detail/ViewModels/DetailViewModel.swift
+++ CryptoTracker/Core/Detail/ViewModels/DetailViewModel.swift
@@ -1,8 +1,23 @@
 
 
 import Foundation
+import Combine
 
-
-class DetailViewModel {
+class DetailViewModel: ObservableObject {
     
+    private let coinDetailService: CoinDetailDataService
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(coin: CoinModel) {
+        self.coinDetailService = CoinDetailDataService(coin: coin)
+    }
+    
+    private func addSubscribers() {
+        coinDetailService.$coinDetails
+            .sink { returnedCoinDetails in
+                print("Received coin detail data")
+                print(returnedCoinDetails)
+            }
+            .store(in: &cancellables)
+    }
 }
